3f6b7352f21f9be528c831190b41d76d845eb10b
kolommik <mikhail.kolomasov@gmail.com>
2024-07-05 21:44:33 +0300

    json save file

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ from backend.diarization_processing import process_diarization
 from backend.merge_results import merge_results
 from logging_config import logger
 import torch
+import json
 
 # Инициализация сессионного состояния
 if "uploaded_file" not in st.session_state:
@@ -17,6 +18,8 @@ if "audio_path" not in st.session_state:
     st.session_state.audio_path = None
 if "processing_times" not in st.session_state:
     st.session_state.processing_times = {}
+if "base_filename" not in st.session_state:
+    st.session_state.base_filename = None
 
 st.title("Speech Scriber")
 
@@ -47,6 +50,7 @@ if uploaded_file is not None:
         f.write(uploaded_file.getbuffer())
 
     st.session_state.audio_path = audio_path
+    st.session_state.base_filename = os.path.splitext(uploaded_file.name)[0]
     st.write("Файл загружен. Начинаем обработку...")
     logger.info(f"Файл {uploaded_file.name} загружен и сохранен в {audio_path}")
 
@@ -77,6 +81,8 @@ if uploaded_file is not None:
         # Объединение результатов
         start_time = time.time()
         result_df = merge_results(whisper_df, diarization_df)
+        result_df["start"] = result_df["start"].round(1)
+        result_df["end"] = result_df["end"].round(1)
         merge_time = time.time() - start_time
         st.session_state.processing_times["Merge"] = merge_time
         st.session_state.result_df = result_df
@@ -90,25 +96,41 @@ if st.session_state.result_df is not None:
     st.write("Результаты:")
     st.dataframe(st.session_state.result_df)
 
-    # Сохранение результатов
+    # Получение имени файла
+    base_filename = st.session_state.base_filename
+
+    # Сохранение результатов в CSV
     result_csv = st.session_state.result_df.to_csv(index=False).encode("utf-8")
-    download_button = st.download_button(
+    download_csv_button = st.download_button(
         label="Скачать результаты в CSV",
         data=result_csv,
-        file_name="results.csv",
+        file_name=f"{base_filename}.csv",
         mime="text/csv",
     )
-    logger.info("Результаты успешно объединены и сохранены.")
+    logger.info("Результаты успешно объединены и сохранены в CSV.")
 
-    # Удаление загруженного файла после скачивания CSV
-    if download_button:
-        os.remove(st.session_state.audio_path)
-        logger.info(
-            f"Файл {st.session_state.audio_path} был удален после скачивания CSV."
-        )
+    # Преобразование DataFrame в список словарей для JSON
+    result_json_data = st.session_state.result_df.apply(
+        lambda row: {"speaker": row["speaker"], "message": row["text"]}, axis=1
+    ).tolist()
+    result_json = json.dumps(result_json_data, ensure_ascii=False, indent=4).encode("utf-8")
+    download_json_button = st.download_button(
+        label="Скачать результаты в JSON",
+        data=result_json,
+        file_name=f"{base_filename}.json",
+        mime="application/json",
+    )
+    logger.info("Результаты успешно объединены и сохранены в JSON.")
+
+    # Кнопка для удаления загруженного файла
+    if st.button("Удалить файл"):
+        if st.session_state.audio_path and os.path.exists(st.session_state.audio_path):
+            os.remove(st.session_state.audio_path)
+            logger.info(f"Файл {st.session_state.audio_path} был удален.")
         st.session_state.uploaded_file = None
         st.session_state.result_df = None
         st.session_state.audio_path = None
+        st.write("Файл был удален.")
 
 # Отображение времени обработки
 if "processing_times" in st.session_state and st.session_state.processing_times:
