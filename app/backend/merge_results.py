a2e254d9189545e28b28ee702998f0342d051115
kolommik <mikhail.kolomasov@gmail.com>
2024-07-05 21:43:37 +0300

    new merging files

--- app/backend/merge_results.py
+++ app/backend/merge_results.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+
 def merge_results(whisper_df, diarization_df):
     # Объединение результатов
     def find_speaker(start, end):
@@ -11,4 +14,28 @@ def merge_results(whisper_df, diarization_df):
     whisper_df["speaker"] = whisper_df.apply(
         lambda row: find_speaker(row["start"], row["end"]), axis=1
     )
-    return whisper_df
+
+    # Объединение строк для одного и того же спикера
+    merged_df = merge_speaker_segments(whisper_df)
+    return merged_df
+
+
+def merge_speaker_segments(df):
+    merged_segments = []
+    current_segment = None
+
+    for _, row in df.iterrows():
+        if current_segment is None:
+            current_segment = row
+        else:
+            if row["speaker"] == current_segment["speaker"]:
+                current_segment["end"] = row["end"]
+                current_segment["text"] += " " + row["text"]
+            else:
+                merged_segments.append(current_segment)
+                current_segment = row
+
+    if current_segment is not None:
+        merged_segments.append(current_segment)
+
+    return pd.DataFrame(merged_segments)
